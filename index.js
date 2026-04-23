3a3522f017b3e6da617c4a2c8954fad5666b6060
muriukielvis24-spec <muriukielvis24@gmail.com>
2026-04-23 22:01:33 +0300

    pass

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-
-
-
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+// First we create a function that takes in multiple arrays of usernames as arguments.
+// The rest parameter syntax allows an arbitrary number of arrays to be passed in.
+function combineUsers(...users) {
+  const mergedArray = users.flat();
+  const now = new Date();
+  const mergeDate = `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`;
+
+  return {
+    users: mergedArray,
+    merge_date: mergeDate,
+  };
+}
+console.log(combineUsers(["Jim3","Pam5","Dwight77"],["Michael6","Eleanor22","Chidi202"],["Jack_jack","Julia_Oreo", "Bill_bore"]));
+ 
